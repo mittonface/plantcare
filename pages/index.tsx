a7d857bc7511f8cc250e178d30f94904c92f236e
Brent Mitton <brent.mitton+github@gmail.com>
2020-09-03 21:33:18 -0400

    I hate this

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const plants: Plant[] = [
       "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
 ];
-const Home: React.FC<any> = () => (
+const Home = () => (
   <Layout pageTitle={"Dashboard"}>
     <PlantList plants={plants} />
   </Layout>
