ee013fec96f15ca8bd8931439403d69263d9b2a4
Brent Mitton <brent.mitton+github@gmail.com>
2020-09-03 21:39:08 -0400

    put things in getstaticprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,21 @@ import { Plant } from "../components/PlantItem";
 import PlantList from "../components/PlantList";
 import React from "react";
 
-const plants: Plant[] = [
-  {
-    name: `Plant1`,
-    plantType: "A good plant",
-    hydrationLevel: 50,
-    lit: false,
-    img:
-      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-];
-const Home = () => (
+export async function getStaticProps() {
+  const plants: Plant[] = [
+    {
+      name: `Plant1`,
+      plantType: "A good plant",
+      hydrationLevel: 50,
+      lit: false,
+      img:
+        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  ];
+  return { props: { plants } };
+}
+
+const Home = ({ plants }: { plants: Plant[] }) => (
   <Layout pageTitle={"Dashboard"}>
     <PlantList plants={plants} />
   </Layout>
